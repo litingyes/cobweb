57535baad8cdb8cc2c291a9efb10e492bf4e5a6a
liting <luz.liting@gmail.com>
2024-11-24 20:13:13 +0800

    feat(crawler): add bing trending images

--- crawler/bing.py
+++ crawler/bing.py
@@ -1,5 +1,5 @@
 from json import dumps
-from os import path
+from os import environ, path
 from utils import ensure_path_exists, get_database_path, getToday
 from requests import get
 
@@ -18,8 +18,26 @@ def pull_daily_wallpaper():
         target_file = path.join(target_dir, mkt + ".json")
         ensure_path_exists(target_file)
         with open(target_file, "w") as f:
-            f.write(dumps(data, ensure_ascii=True, indent=2))
+            f.write(dumps(data, ensure_ascii=False, indent=2))
 
+def pull_trending_images():
+    target_dir = path.join(get_database_path(), "bing", "trending-images", getToday())
+    key = environ.get('AZURE_SUBSCRIPTION_KEY')
+    
+    for mkt in MKTs:
+        r = get(
+            "https://api.bing.microsoft.com/v7.0/images/trending",
+            headers={'Ocp-Apim-Subscription-Key':key},
+            params={'safeSearch':'Strict', 'count':8,'mkt':mkt}
+        )
+        data = r.json()["categories"]
+        target_file = path.join(target_dir, mkt + ".json")
+        ensure_path_exists(target_file)
+        with open(target_file, "w") as f:
+            f.write(dumps(data, ensure_ascii=False, indent=2))
+    
+    
 
 def pull_from_bing():
     pull_daily_wallpaper()
+    pull_trending_images()
