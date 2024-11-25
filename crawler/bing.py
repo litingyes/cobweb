a727b2db1de186ef2e438274675d7d20b74b0362
liting <luz.liting@gmail.com>
2024-11-26 00:15:37 +0800

    feat(crawler): adjust readme struct

--- crawler/bing.py
+++ crawler/bing.py
@@ -9,7 +9,6 @@ MKTs = ["en-US", "zh-CN"]
 
 def pull_daily_wallpaper():
     target_dir = path.join(get_database_path(), "bing", "daily-wallpaper")
-    md_content = "\n| United States (en-US) | China (zh-CN) |\n| :----: | :----: |\n"
 
     for mkt in MKTs:
         r = get(
@@ -25,18 +24,20 @@ def pull_daily_wallpaper():
         first_image = data[0]
         url = BING_DOMAIN + first_image["url"]
         uhd_url = BING_DOMAIN + first_image["urlbase"] + "_UHD.jpg"
-        md_content += (
-            f"| [![{first_image['title']}]({url}) {first_image['title']}]({uhd_url})"
-        )
+        
+        if (mkt == 'en-US'):
+            md_content = (
+                f"![{first_image['title']}]({url}) Today: [{first_image['title']}]({uhd_url})"
+            )
+            update_readme("BING_DAILY_WALLPAPER", md_content)
+            
 
-    md_content += "|\n"
-    update_readme("BING_DAILY_WALLPAPER", md_content)
 
 
 def pull_trending_images():
     target_dir = path.join(get_database_path(), "bing", "trending-images")
     key = environ.get("AZURE_SUBSCRIPTION_KEY")
-    md_content = "\n| United States (en-US) | China (zh-CN) |\n| :----: | :----: |\n"
+    md_content = "\n|Popular nature and landmark | Popular wallpaper |\n| :----: | :----: |\n"
 
     for mkt in MKTs:
         r = get(
@@ -49,13 +50,17 @@ def pull_trending_images():
         ensure_path_exists(target_file)
         with open(target_file, "w") as f:
             f.write(dumps(data, ensure_ascii=False, indent=2))
-
-        description = data[0]["tiles"][0]["query"]["displayText"]
-        url = data[0]["tiles"][0]["image"]["thumbnailUrl"]
-        md_content += f"| [![{description}]({url}) {description}]({url})"
-
-    md_content += "|\n"
-    update_readme("BING_TRENDING_IMAGES", md_content)
+        
+        if (mkt == 'en-US'):
+            for i in range(8):
+                nature_or_landmark = data[2]['tiles'][i]
+                nature_or_landmark_description = nature_or_landmark["query"]["displayText"]
+                nature_or_landmark_url = nature_or_landmark["image"]["thumbnailUrl"]
+                wallpaper = data[3]['tiles'][i]
+                wallpaper_description =  wallpaper["query"]["displayText"]
+                wallpaper_url =  wallpaper["image"]["thumbnailUrl"]
+                md_content += f"| [![{nature_or_landmark_description}]({nature_or_landmark_url}) {nature_or_landmark_description}]({nature_or_landmark_url}) | [![{wallpaper_description}]({wallpaper_url}) {wallpaper_description}]({wallpaper_url}) |\n"
+            update_readme("BING_TRENDING_IMAGES", md_content)
 
 
 def pull_from_bing():
