ec3244c25e8f2eb4787189e441433ffe64e8a312
liting <luz.liting@gmail.com>
2024-11-27 00:23:51 +0800

    feat(header): click logo/title to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,12 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className="fixed top-0 z-20 flex h-14 w-full items-center justify-between px-16 backdrop-blur-md">
-      <div className="flex items-center gap-4">
-        <Image src="/images/icon-512.png" alt="logo" width={32} height={32} />
-        <div className="text-3xl font-bold">cobweb</div>
-      </div>
+      <Link href="/">
+        <div className="flex items-center gap-4">
+          <Image src="/images/icon-512.png" alt="logo" width={32} height={32} />
+          <div className="text-3xl font-bold">cobweb</div>
+        </div>
+      </Link>
       <div className="flex items-center gap-2">
         <Link href="/images">
           <Button className="text-xl" variant="light" size="sm">
